fe432a7dfe0755b4f26f1138731921d1b1708270
Jonathan Ochaya <wangolojowel@gmail.com>
2022-08-31 12:53:54 +0300

    Added POST method for storing new customer, added PUT/PATCH methods for modifying customer data - used custom request methods for handling both context cases

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api\V1;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\StoreCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Services\V1\CustomerFilter;
@@ -13,21 +15,17 @@ class CustomerController extends Controller
 {
     public function index(CustomerFilter $filter)
     {
-        $queryItems = $filter->transform(request());
+        $customers = Customer::where($filter->transform(request()));
 
-        $includeInvoices = request()->query('includeInvoices');
-
-        $customers = Customer::where($queryItems);
-
-        if($includeInvoices)
+        if(request()->query('includeInvoices'))
             $customers->with('invoices');
 
         return new CustomerCollection($customers->paginate());
     }
 
-    public function store(Request $request)
+    public function store(StoreCustomerRequest $request)
     {
-        //
+        return new CustomerResource(Customer::create($request->all()));
     }
 
     public function show(Customer $customer)
@@ -38,9 +36,9 @@ class CustomerController extends Controller
         return new CustomerResource($customer);
     }
 
-    public function update(Request $request, Customer $customer)
+    public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     public function destroy(Customer $customer)
